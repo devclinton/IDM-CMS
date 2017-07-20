56bf6cc3da55a5c1b7bd1c1fd36b0a4635219435
mroh <min.roh@gmail.com>
2017-07-19 21:56:03 -0700

    Removed Min's previous research files
    Removed Min's previous research files under products/page

--- products/papers/Rare Event Detection with Non-perturbable Reactions/updateRates.m
+++
@@ -1,9 +0,0 @@
-% function to update rate
-
-function [k, gamma] = updateRates(reaction_constant_vector, original_rcv, index_of_update, gamma, lambda)
-
-k = reaction_constant_vector;
-k_original = original_rcv;
-k_proposed = k_original(index_of_update) ./ gamma(index_of_update);
-k(index_of_update) = k(index_of_update) * (1-lambda) + k_proposed * lambda;
-        
--- products/papers/Rare Event Detection with Non-perturbable Reactions/updateRates_v4.m
+++
@@ -1,6 +0,0 @@
-% function to update rate
-function k = updateRates_v4(k, k_default, index_of_update, gamma, lambda)
-
-k_proposed = k_default(index_of_update) ./ gamma{index_of_update};
-k(index_of_update) = k(index_of_update) * (1-lambda) + k_proposed * lambda;
-
